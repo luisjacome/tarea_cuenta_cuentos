713aa5a90f724bb4667d6902b4957b0f9604a127
luisjacome <luis1987_m@live.com>
2016-03-25 19:40:11 -0500

    cuenta cuentos con una frase

---
+++ scripts.js
@@ -0,0 +1,14 @@
+
+
+console.log("Inicio de Programa");
+	var sustantivo =prompt("Ingrese un sustantivo?");
+	var adjetivo =prompt("Ingrese un adjetivo?");
+	var verbo =prompt("Ingrese un verbo?");
+	document.write("<h1>Te cuento el cuento: </h1>");
+	document.write("<p>Se dice que el " +sustantivo+ " es una " +adjetivo+ " hecha para  " +verbo+ ".</p>");
+	
+console.log("Fin de Programa");
+
+
+
+
