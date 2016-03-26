bf5a6b96d2c5532fbafd507a2eb192298ac1ef0f
luisjacome <luis1987_m@live.com>
2016-03-25 20:00:41 -0500

    cuenta cuentos con tres frases

--- scripts.js
+++ scripts.js
@@ -6,7 +6,8 @@ console.log("Inicio de Programa");
 	var verbo =prompt("Ingrese un verbo?");
 	document.write("<h1>Te cuento el cuento: </h1>");
 	document.write("<p>Se dice que el " +sustantivo+ " es una " +adjetivo+ " hecha para  " +verbo+ ".</p>");
-	document.write("<p>El " +verbo+  " del " +sustantivo+ " afecta sus cinco sentidos que lo hacen una " +adjetivo+ " perfeca.</p>");
+	document.write("<p>El " +verbo+  " del " +sustantivo+ " afecta sus cinco sentidos que lo hacen una " +adjetivo+ " perfecta.</p>");
+	document.write("<p>Al verse como una " +adjetivo+ " vulnerable al " +verbo+ " trata que el sufrir no afecte su cuerpo de " +sustantivo+ ".</p>");
 console.log("Fin de Programa");
 
 
