7ca02b40ca6d8ec41fd49d32e1ad73c196a7ec90
luisjacome <luis1987_m@live.com>
2016-03-25 19:45:59 -0500

    cuenta cuentos con dos frases

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ console.log("Inicio de Programa");
 	var verbo =prompt("Ingrese un verbo?");
 	document.write("<h1>Te cuento el cuento: </h1>");
 	document.write("<p>Se dice que el " +sustantivo+ " es una " +adjetivo+ " hecha para  " +verbo+ ".</p>");
-	
+	document.write("<p>El " +verbo+  " del " +sustantivo+ " afecta sus cinco sentidos que lo hacen una " +adjetivo+ " perfeca.</p>");
 console.log("Fin de Programa");
 
 
